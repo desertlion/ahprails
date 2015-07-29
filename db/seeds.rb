c79b8c4b874958fa4adaf06f23bbe972840a732b
Rijalul Fikri <fikri.desertlion@gmail.com>
2015-07-29 21:08:34 +0700

    Editing interpolasi

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+r1 = Role.create({name: "Regular", description: "Can read items"})
+r2 = Role.create({name: "Admin", description: "Can perform any CRUD operation on any resource"})
